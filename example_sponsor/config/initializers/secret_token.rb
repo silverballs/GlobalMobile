944d464a79242db075317ddb4beb497269378cd7
Silverballs <asilbersmith11@gmail.com>
2013-10-05 14:27:36 -0400

    Finish example event model

---
+++ example_sponsor/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExampleSponsor::Application.config.secret_key_base = '8bd9832d64423c4b49ff9331318a9a73114e50dbbbaa14c4fb5b9c11ce18738e2ef7930ba12990564f42fbfa720361f5180839c2de370f738963c8ac1956245b'
